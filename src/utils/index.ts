9fdd67d575ccfbff5342bba3be791f33571b7370
Omid Astaraki <omid.ocean@gmail.com>
2020-05-10 19:39:35 +0430

    added jsdoc for getView function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,13 @@ import { ViewType } from 'types/data';
 
 export const isServer = typeof window === 'undefined';
 
+/**
+ * returns a view type based on screen width
+ *
+ * @export
+ * @param width screen width
+ * @returns screen type
+ */
 export function getView(width: number): ViewType {
   let newView: ViewType = 'MobileView';
   if (width > 1220) {

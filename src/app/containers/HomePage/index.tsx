0f30f03f7a23a889fadf882556d0ed6381102530
Omid Astaraki <omid.ocean@gmail.com>
2020-05-14 08:40:43 +0430

    Homepage improvements

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -9,44 +9,64 @@ export function Home() {
   return (
     <PageContainer>
       <Row gutter={[16, 16]}>
-        <Col lg={8} md={12} sm={24} xs={24}>
+        <Col lg={6} md={12} sm={24} xs={24}>
           <Sticker
             backgroundColor="#1abc9c"
             fontColor="#fff"
-            description="someDesc"
+            description="سطح دسترسی"
             value={3}
             icon={<StarFilled />}
           />
         </Col>
-        <Col lg={8} md={12} sm={24} xs={24}>
+        <Col lg={6} md={12} sm={24} xs={24}>
           <Sticker
             backgroundColor="#8e44ad"
             fontColor="#fff"
-            description="someDesc"
+            description="کل بیمه های ثبت شده"
             value={3}
             icon={<StarFilled />}
           />
         </Col>
-        <Col lg={8} md={12} sm={24} xs={24}>
+        <Col lg={6} md={12} sm={24} xs={24}>
           <Sticker
             backgroundColor="#2c3e50"
             fontColor="#fff"
-            description="someDesc"
+            description="کل افراد بیمه شده"
+            value={3}
+            icon={<StarFilled />}
+          />
+        </Col>
+        <Col lg={6} md={12} sm={24} xs={24}>
+          <Sticker
+            backgroundColor="#2c3e50"
+            fontColor="#fff"
+            description="کل افراد بیمه شده"
             value={3}
             icon={<StarFilled />}
           />
         </Col>
       </Row>
-      <Row>
-        <Col lg={12}>
+      <Row gutter={[16, 16]}>
+        <Col lg={8} md={24} sm={24} xs={24}>
           <LineChartWidget
             height={500}
             data={chartData.homepage}
-            title="چارت شماره 1"
+            title="فروش های نماینده"
           />
-        </Col>{' '}
-        <Col lg={12}>
-          <LineChartWidget height={500} data={chartData.homepage} />
+        </Col>
+        <Col lg={8} md={24} sm={24} xs={24}>
+          <LineChartWidget
+            title="بیمه های ثبت شده"
+            height={500}
+            data={chartData.homepage}
+          />
+        </Col>
+        <Col lg={8} md={24} sm={24} xs={24}>
+          <LineChartWidget
+            title="پورسانت بیمه گذار"
+            height={500}
+            data={chartData.homepage}
+          />
         </Col>
       </Row>
     </PageContainer>
